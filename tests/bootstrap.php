10bea9013946207126810a5e21b78af9530dcb79
Jérôme Gamez <jerome@gamez.name>
2022-03-24 03:07:35 +0100

    Remove obsolete files and directories
    It should be clear that composer dependencies need to be installed
    in order to work on and test the library, so the `bootstrap.php` 
    file can be deleted.
    
    Since USAGE.md has been merged into README.md, the `site` 
    directory is not needed anymore.

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!file_exists($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-    throw new RuntimeException('Install dependencies to run test suite.');
-}
-
-require_once $autoloadFile;
